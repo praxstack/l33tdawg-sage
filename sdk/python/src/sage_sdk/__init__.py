bf251c25394800bec0f832458ed5fe7913361d7c
Dhillon Kannabhiran <l33tdawg@hackinthebox.org>
2026-04-18 16:26:45 +0800

    chore: align Python SDK version with SAGE release (6.5.4)

--- sdk/python/src/sage_sdk/__init__.py
+++ sdk/python/src/sage_sdk/__init__.py
@@ -4,5 +4,5 @@ from sage_sdk.auth import AgentIdentity
 from sage_sdk.async_client import AsyncSageClient
 from sage_sdk.client import SageClient
 
-__version__ = "6.5.3"
+__version__ = "6.5.4"
 __all__ = ["SageClient", "AsyncSageClient", "AgentIdentity"]
